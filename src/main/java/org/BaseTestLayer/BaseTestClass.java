1ddd0e3704f18922d8edf2aece2d33d7f82a7fa5
saipraneethkammela <saipraneeth.kammela@gmail.com>
2025-03-16 16:43:06 +0530

    initial commits

--- src/main/java/org/BaseTestLayer/BaseTestClass.java
+++ src/main/java/org/BaseTestLayer/BaseTestClass.java
@@ -1,6 +1,6 @@
-package org.example;
+package org.BaseTestLayer;
 
-public class Main {
+public class BaseTestClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/test/java/com/PageTestLayer/TestClass1.java
@@ -0,0 +1,12 @@
+package com.PageTestLayer;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+    @Test
+    public void testCase1()
+    {
+
+    }
+}
